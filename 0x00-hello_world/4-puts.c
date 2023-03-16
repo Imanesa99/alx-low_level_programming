094cd7783e570d6b841b7287bff92d257800dc8f
Imanesa99 <imane.sabah32@gmail.com>
2023-03-16 13:59:13 -0700

    4-puts.c task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is main function
+ * Return: alawys 0
+ */
+int main(void)
+{
+ puts("\"Programming is like building a multilingual puzzle);
+ return(0);
+
+}
